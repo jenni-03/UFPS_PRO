321cdc841a3ce0072da9056e95efefde56c119dd
Jaip3r <jaidergustavoolmo@ufps.edu.co>
2023-09-13 20:52:22 -0500

    Adición de pool de conexiones, cambio de logger y transición a ESModules

--- src/server.js
+++ src/server.js
@@ -1,25 +1,25 @@
-const express = require('express');
-const {logger, logEvents} = require('./middlewares/logger');
-const {errorHandler} = require('./middlewares/errorHandler');
-const sequelize = require('./database/db');
-const cookieParser = require('cookie-parser');
-const cors = require('cors');
-const corsOptions = require('./util/corsOptions');
-const path = require('path');
-const generateRole = require('./util/generateRole');
-const createAdminUser = require('./util/createAdminUser');
+import express from 'express';
+import logger from './middlewares/logger.js';
+// const {errorHandler} = require('./middlewares/errorHandler');
+import sequelize from './database/db.js';
+import cookieParser from 'cookie-parser';
+import cors from 'cors';
+// const corsOptions = require('./util/corsOptions');
+import path from 'node:path';
+// const generateRole = require('./util/generateRole');
+// const createAdminUser = require('./util/createAdminUser');
 
 // Importamos las tablas a crear
-require('./database/associations');
+// require('./database/associations');
 
 // Importar Rutas de la API
-const authRoutes = require('./routes/auth.routes');
+/*const authRoutes = require('./routes/auth.routes');
 const userRoutes = require('./routes/user.routes');
 const preguntasRoutes = require('./routes/preguntas.routes');
 const competenciaRoutes = require('./routes/competencia.routes');
 const categoriaRoutes = require('./routes/categoria.routes');
 const pruebaRoutes = require('./routes/prueba.routes');
-const convocatoriaRoutes = require('./routes/convocatoria.routes');
+const convocatoriaRoutes = require('./routes/convocatoria.routes');*/
 
 // Inicializar el contexto principal
 const app = express();
@@ -28,27 +28,26 @@ const app = express();
 const PORT = process.env.PORT || 3500;
 
 // Especificamos el directorio de archivos estáticos
-app.use(express.static(path.resolve(__dirname, './public')));
+/*app.use(express.static(path.resolve(__dirname, './public')));
 
 app.use('directors', express.static(path.resolve(__dirname, './public/directors')));
 
-app.use('questions', express.static(path.resolve(__dirname, './public/questions')));
+app.use('questions', express.static(path.resolve(__dirname, './public/questions')));*/
 
 // Middlwares
-app.use(logger);
-app.use(cors(corsOptions));
+//app.use(cors(corsOptions));
 app.use(express.json());
 app.use(cookieParser());
 
 
 // Rutas
-app.use('/api/auth', authRoutes);
+/*app.use('/api/auth', authRoutes);
 app.use('/api/user', userRoutes);
 app.use('/api/question', preguntasRoutes);
 app.use('/api/competencia', competenciaRoutes);
 app.use('/api/categoria', categoriaRoutes);
 app.use('/api/prueba', pruebaRoutes);
-app.use('/api/convocatoria', convocatoriaRoutes);
+app.use('/api/convocatoria', convocatoriaRoutes);*/
 
 // En caso de acceder a una ruta no especificada
 app.all('*', (req, res) => {
@@ -64,26 +63,26 @@ app.all('*', (req, res) => {
 });
 
 // Middleware de manejo de errores
-app.use(errorHandler);
+//app.use(errorHandler);
 
 
 // Corremos el servidor
-sequelize.sync()
-    .then(() => {
+sequelize.authenticate()
+    /*.then(() => {
 
         // Creamos los roles y el usuario admin
         return Promise.all([generateRole(), createAdminUser()]);
 
-    })
+    })*/
     .then(() => {
 
         // Iniciamos el servidor una vez que las tablas se sincronizen
         app.listen(PORT, () => {
-            console.log('Server listening on port', PORT);
+            logger.info(`App is running on http://localhost:${PORT}`);
         });
 
     })
-    .catch(err => console.error('Unable to connect to the database', err));
+    .catch(err => logger.error(`Error al sincronizar con la BD: ${err.message}`));
 
 
 
