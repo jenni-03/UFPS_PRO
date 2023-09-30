c52a965a519d676f5cfd087f799aa6a63e221eff
jenni-03 <jenniferdayanasr@ufps.edu.co>
2023-09-29 20:54:14 -0500

    Esquema de Competencia, y cambios en el controlador

--- src/routes/competencia.routes.js
+++ src/routes/competencia.routes.js
@@ -7,6 +7,7 @@ import competenciaController from '../controllers/competenciaController.js';
 import extractToken from '../middlewares/extractToken.js';
 import authJWT from '../middlewares/verifyJWT.js';
 import isAdmin from '../middlewares/isAdmin.js';
+import {validateCompetenceData} from '../schemas/competenceSchema.js'
 
 // Inicializamos el router
 const router = Router();
@@ -17,31 +18,31 @@ const router = Router();
 // @desc Endpoint encargado de la obtención de todas las competencias activas
 // @route GET /api/competencia
 // @access solo Admin
-router.get('/', [authJWT, isAdmin], competenciaController.getCompetencias);
+router.get('/', [extractToken, authJWT, isAdmin, validateCompetenceData], competenciaController.getCompetencias);
 
 
 // @desc Endpoint encargado de la obtención de una competencia por id
 // @route GET /api/competencia/:id
 // @access solo Admin
-router.get('/:id', [authJWT, isAdmin], competenciaController.getCompetenciaById);
+router.get('/:id', [extractToken, authJWT, isAdmin, validateCompetenceData], competenciaController.getCompetenciaById);
 
 
 // @desc Endpoint encargado de la obtención de las categorias activas asociadas a una competencia
 // @route GET /api/competencia/:id/categorias
 // @access solo Admin
-router.get('/:id/categorias', [authJWT, isAdmin], competenciaController.getCategoriasCompetencia);
+router.get('/:id/categorias', [extractToken, authJWT, isAdmin, validateCompetenceData], competenciaController.getCategoriasCompetencia);
 
 
 // @desc Endpoint encargado de la creación de una competencia
 // @route POST /api/competencia/create
 // @access solo Admin
-router.post('/create', [extractToken, authJWT, isAdmin], competenciaController.createCompetencia);
+router.post('/create', [extractToken, authJWT, isAdmin, validateCompetenceData], competenciaController.createCompetencia);
 
 
 // @desc Endpoint encargado de la actualización de una competencia
 // @route POST /api/competencia/update/:id
 // @access solo Admin
-router.put('/update/:id', [authJWT, isAdmin], competenciaController.updateCompetencia);
+router.put('/update/:id', [extractToken, authJWT, isAdmin, validateCompetenceData], competenciaController.updateCompetencia);
 
 // Exportamos el router
 export default router;
