6978826ee19e0d85512f65b5ec62867f554ff87b
Jaip3r <jaidergustavoolmo@ufps.edu.co>
2023-12-04 21:52:35 -0500

    Acomodación controlador de gestión de progreso prueba estudiante, evitar repetidos2

--- src/controllers/convocatoriaController.js
+++ src/controllers/convocatoriaController.js
@@ -712,24 +712,22 @@ const setProgresoEstudiante = async (req, res, next) => {
         // Obtenemos el id de la pregunta, el tiempo y la opcion seleccionada
         const { id_pregunta, tiempo,  opcion } = req.body;
 
-        // Obtenemos la inscripción del estudiante a la prueba y Verificamos si el usuario ya tiene registrado una respuesta a esa pregunta
-        const [ inscripcion, respuesta ] = await Promise.all([
+        // Obtenemos la inscripción del estudiante a la prueba y 
+        const inscripcion = await Inscripcion.findOne({ 
+            where: {
+                usuario_id: userId,
+                convocatoria_id: id
+            }
+        });
 
-            Inscripcion.findOne({ 
-                where: {
-                    usuario_id: userId,
-                    convocatoria_id: id
-                }
-            }),
-            Respuesta.findOne({
-                where: {
-                    inscripcion_id: inscripcion.id,
-                    pregunta_id: id_pregunta
-                }
-            })
-            
-            
-        ])
+        // Verificamos si el usuario ya tiene registrado una respuesta a esa pregunta
+        const respuesta = await Respuesta.findOne({
+            where: {
+                inscripcion_id: inscripcion.id,
+                pregunta_id: id_pregunta
+            }
+        });
+
         
         // Verificamos que el usuario haya seleccionado una opción
         if (opcion !== null) {
